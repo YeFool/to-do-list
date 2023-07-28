834b5f954e769f5711cd98d40dbc75d131f8922c
Benjamin Howe <yefool@hotmail.co.uk>
2023-07-28 17:14:14 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [newToDo, setNewToDo] = useState("");
+  const [addToDo, setAddToDo] = useState([]);
+
+  const addItem = (item, buttons) => {
+    if (newToDo === "") {
+      alert("Enter an Item");
+      return;
+    }
+    let newItem = [...addToDo];
+    newItem.push({ text: item, buttons, completed: false });
+    setAddToDo(newItem);
+    setNewToDo("");
+  };
+
+  const remove = (index) => {
+    let newItem = [...addToDo];
+    newItem.splice(index, 1);
+    setAddToDo(newItem);
+  };
+
+  const complete = (index) => {
+    let newItem = [...addToDo];
+    newItem[index].completed = !newItem[index].completed;
+    setAddToDo(newItem);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>To do list</h1>
+
+      <input
+        type="text"
+        placeholder="What needs doing?"
+        value={newToDo}
+        onChange={(e) => setNewToDo(e.target.value)}
+      ></input>
+
+      <button
+        onClick={() => {
+          addItem(newToDo, ["Complete", "Delete"]);
+        }}
+      >
+        +
+      </button>
+
+      {addToDo.map((item, index) => {
+        return (
+          <div key={index}>
+            <ul className={item.completed ? "complete" : ""}>{item.text}</ul>
+            <div>
+              {item.buttons.map((buttonText, buttonIndex) => (
+                <button
+                  key={buttonIndex}
+                  onClick={() => {
+                    if (buttonText === "Complete") {
+                      complete(index);
+                    } else if (buttonText === "Delete") {
+                      remove(index);
+                    }
+                  }}
+                >
+                  {buttonText}
+                </button>
+              ))}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
